be93ec883c9a60a2a78a83a352d68745bdf28416
YuriyMikh <pamukkale1717@gmail.com>
2022-11-30 23:22:24 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,41 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listElements = document.querySelector("#ingredients");
+
+const addIngredients = ingredients.map((ingredient) => {
+  const newElement = document.createElement("li");
+  newElement.textContent = ingredient;
+  newElement.classList.add("item");
+
+  return newElement;
+});
+
+listElements.append(...addIngredients);
+
+//----------------
+//решение через функцию
+// const listElements = document.querySelector("#ingredients");
+
+// const addIngredients = array => {
+//   return array.map(ingredient => {
+//   const newElement = document.createElement("li");
+//   newElement.textContent = ingredient;
+//   newElement.classList.add("item");
+
+//   return newElement;
+// })
+// };
+
+// const functionСall = addIngredients(ingredients); //вызов функции
+
+// listElements.append(...functionСall);
+
+
+
