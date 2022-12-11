1a76ddfae919f2c596616cb0f291fbf84167719f
YuriyMikh <pamukkale1717@gmail.com>
2022-12-11 22:39:59 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,11 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
-inputEl.addEventListener("input", (event) => {
-    outputEl.textContent = event.currentTarget.value;
+inputEl.addEventListener("input", onInputText);
 
-    if (event.currentTarget.value === "") {
-      outputEl.textContent = "Anonymous";
-    } 
-});
-
+function onInputText(event) {
+  outputEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    outputEl.textContent = "Anonymous";
+  }
+}
