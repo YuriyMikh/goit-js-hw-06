cbc5b11132eae8b8397e04af2b28f2225b3b8ab8
YuriyMikh <pamukkale1717@gmail.com>
2022-12-11 23:39:14 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -7,5 +7,7 @@ function onInputText(event) {
   outputEl.textContent = event.currentTarget.value;
   if (event.currentTarget.value === "") {
     outputEl.textContent = "Anonymous";
+    //outputEl.textContent = outputEl.textContent;
+    //вариант не со строкой "Anonymous", а стем что изначально было в outputEl.textContent
   }
 }
