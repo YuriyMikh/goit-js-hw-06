de0296f3423a25e45e713b3dcaf8fa7ea4a07816
YuriyMikh <pamukkale1717@gmail.com>
2022-12-16 01:47:49 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const inputArea = document.querySelector("#validation-input");
+
+inputArea.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+  if (event.currentTarget.value.length != inputArea.dataset.length) {
+    console.log("false");
+    inputArea.classList.add("invalid");
+  } else {
+    console.log("true");
+    inputArea.classList.add("valid");
+  }
+}
