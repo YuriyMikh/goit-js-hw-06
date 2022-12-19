9c97aa5ecdb05c0f2c05c4785005b43c5d630d05
YuriyMikh <pamukkale1717@gmail.com>
2022-12-20 01:54:34 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormChange);
+
+function onFormChange(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const formData = { email, password };
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
