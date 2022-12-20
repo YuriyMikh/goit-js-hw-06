b88f8adca7bbaad5245c6bb5c6df1b0e09a1aa56
YuriyMikh <pamukkale1717@gmail.com>
2022-12-20 23:09:02 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const buttonChangeColor = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+buttonChangeColor.addEventListener("click", onChangeColorBody);
+
+function onChangeColorBody() {
+  const changeColorBody = (document.body.style.backgroundColor =
+    getRandomHexColor());
+  colorSpan.textContent = changeColorBody;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
