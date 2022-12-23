57272bbd66baac337a606eb41bf6731ff8ff7bdd
YuriyMikh <pamukkale1717@gmail.com>
2022-12-23 02:38:24 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,4 @@
+const divControlsRef = document.querySelector("#controls");
 const inputRef = document.querySelector("input");
 const buttonCreateRef = document.querySelector("[data-create]");
 const buttonDestoyRef = document.querySelector("[data-destroy]");
@@ -9,21 +10,19 @@ buttonDestoyRef.addEventListener("click", destroyBoxes);
 function createBoxes(amount) {
   amount = inputRef.value;
   console.log(amount);
-  if (inputRef === "") return;
-  else {
-    const divElement = document.createElement("div");
-    divElement.style.backgroundColor = getRandomHexColor();
-    divElement.style.height = "30px";
-    divElement.style.width = "30px";
-    divBoxesRef.append(divElement);
-  }
 
-  // divBoxesRef.insertAdjacentHTML("afterbegin", divElement);
+  const divElement = document.createElement("div");
+  divElement.style.backgroundColor = getRandomHexColor();
+  divElement.style.height = "30px";
+  divElement.style.width = "30px";
+  // divBoxesRef.append(divElement);
+
+  divBoxesRef.insertAdjacentHTML("afterbegin", '<div>TEST!!!</div>');
 }
 
 function destroyBoxes(event) {
   divBoxesRef.innerHTML = "";
-  inputRef.currentTarget.reset();
+  // event.currentTarget.reset();
 }
 
 function getRandomHexColor() {
