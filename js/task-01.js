63f83f9dc708f6e40e2f931db738d9c25b909990
YuriyMikh <pamukkale1717@gmail.com>
2022-12-24 14:57:25 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,9 @@
+const categoriesItemRef = document.querySelectorAll("#categories .item");
+console.log("Number of categories:", categoriesItemRef.length);
 
-//first task
-const categories = document.querySelectorAll('#categories .item');
-console.log('Number of categories:', categories.length);
-
-// //second task
-const firstCategory = document.querySelector('.item:first-child h2').textContent;
-const quantityElementsFirstCategory = document.querySelectorAll('.item:first-child li').length;
-console.log(`Category: ${firstCategory}`);
-console.log(`Elements: ${quantityElementsFirstCategory}`);
-
-const secondCategory = document.querySelector('.item:nth-child(2) h2').textContent;
-const quantityElementsSecondCategory = document.querySelectorAll('.item:nth-child(2) li').length;
-console.log(`Category: ${secondCategory}`);
-console.log(`Elements: ${quantityElementsSecondCategory}`);
-
-const thirdCategory = document.querySelector('.item:last-child h2').textContent;
-const quantityElementsThirdCategory = document.querySelectorAll('.item:last-child li').length;
-console.log(`Category: ${thirdCategory}`);
-console.log(`Elements: ${quantityElementsThirdCategory}`);
+categoriesItemRef.forEach((element) => {
+  const categoriesTitle = element.querySelector("h2").textContent;
+  const categoriesQuantity = element.querySelectorAll("li").length;
+  console.log(`Category: ${categoriesTitle}`);
+  console.log(`Elements: ${categoriesQuantity}`);
+});
