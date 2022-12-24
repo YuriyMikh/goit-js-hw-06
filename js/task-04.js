25a4bdf01a32cea3f2fb4664532437a3f61fc996
YuriyMikh <pamukkale1717@gmail.com>
2022-12-24 14:59:19 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,18 +1,18 @@
 //ссылки на элементы DOM с которыми бужем работать
-const decrementButtonEl = document.querySelector('[data-action="decrement"]');
-const incrementButtonEl = document.querySelector('[data-action="increment"]');
-const resultEl = document.querySelector("#value");
+const decrementButtonRef = document.querySelector('[data-action="decrement"]');
+const incrementButtonRef = document.querySelector('[data-action="increment"]');
+const resultRef = document.querySelector("#value");
 
 let counterValue = 0; //переменная для счетчика
 
 //слушатель клика "-1"
-decrementButtonEl.addEventListener("click", () => {
+decrementButtonRef.addEventListener("click", () => {
   counterValue -= 1;
-  resultEl.textContent = counterValue;
+  resultRef.textContent = counterValue;
 });
 
 //слушатель клика "+1"
-incrementButtonEl.addEventListener("click", () => {
+incrementButtonRef.addEventListener("click", () => {
   counterValue += 1;
-  resultEl.textContent = counterValue;
+  resultRef.textContent = counterValue;
 });
