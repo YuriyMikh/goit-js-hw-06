e8d976beed8ab364bd98ee0179fbeaa902e2d3a4
YuriyMikh <pamukkale1717@gmail.com>
2022-12-24 15:03:44 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,15 @@
-const inputArea = document.querySelector("#validation-input");
+const inputAreaRef = document.querySelector("#validation-input");
 
-inputArea.addEventListener("blur", onInputBlur);
+inputAreaRef.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length === +inputArea.dataset.length) {
+  if (event.currentTarget.value.length === +inputAreaRef.dataset.length) {
     // console.log("true"); //для проверочки
-    inputArea.classList.add("valid");
-    inputArea.classList.remove("invalid");
+    inputAreaRef.classList.add("valid");
+    inputAreaRef.classList.remove("invalid");
   } else {
     // console.log("false"); //для проверочки
-    inputArea.classList.add("invalid");
-    inputArea.classList.remove("valid");
+    inputAreaRef.classList.add("invalid");
+    inputAreaRef.classList.remove("valid");
   }
 }
