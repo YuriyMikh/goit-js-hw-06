486f841f2cc7948dda7f10fd0f3e89a94f41f914
YuriyMikh <pamukkale1717@gmail.com>
2023-05-12 21:12:56 +0300

    upd

--- js/task-01.js
+++ js/task-01.js
@@ -4,6 +4,6 @@ console.log(`Number of categories: ${categoriesItemRef.length}`);
 categoriesItemRef.forEach((element) => {
   const categoriesTitle = element.querySelector("h2").textContent;
   const categoriesQuantity = element.querySelectorAll("li").length;
-  console.log(`Category: ${categoriesTitle}`);
-  console.log(`Elements: ${categoriesQuantity}`);
+  console.log(`Category:`, categoriesTitle);
+  console.log(`Elements:`, categoriesQuantity);
 });
